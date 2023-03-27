4513f3bfe4491d7c6a8950fd59411e05a76e5338
JeoingIn1994 <be7814@gmail.com>
2023-03-27 19:15:46 +0900

    update 2023-03-27

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,59 @@
 
-const socket = io('/');
+const socket = io('/chattings');
 const getElementById = (id) => document.getElementById(id) || null;
 
 const helloStrangerElement = getElementById('hello_stragers');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+socket.on('user_connected', (username) => {
+    drawNewChat(`${username} connected`);
+});
+
+socket.on('new_chat',(data) => {
+    const {chat, username} = data;
+    drawNewChat(`${username} : ${chat}`)
+})
+const handleSubmit = (event) =>{
+    event.preventDefault();
+
+    const inputValue = event.target.elements[0].value;
+    
+    if (inputValue !== ""){
+        socket.emit('submit_chat',inputValue);
+
+        drawNewChat(`me : ${inputValue}`);
+        event.target.elements[0].value = '';
+    }
+};
+
+const drawHelloStranger = (username) => {
+    helloStrangerElement.innerText = `Hello ${username} Stranger :)`
+};
+
+const drawNewChat = (massage) =>{
+    const wrapperChatbox = document.createElement('div');
+    const chatBox = `
+        <div>
+            ${massage}
+        </div>
+    `;
+    wrapperChatbox.innerHTML = chatBox;
+    chattingBoxElement.append(wrapperChatbox);
+}
+
 function helloUser(){
     const username = prompt('What is your name?');
-    socket.emit("new_user",username);
-    console.log(username);
-    socket.on("hello_user", (data) => {
-        console.log(data); 
-    })
+    socket.emit("new_user",username,(data) =>{
+        drawHelloStranger(data);
+    });
+    
 }
 
 function init() {
     helloUser();
+
+    formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -3,20 +3,50 @@ import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 
-@WebSocketGateway()
-export class ChatsGateway
-{
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`)
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`)
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket,
   ) {
-    console.log(socket.id);
-    console.log(username);
-    socket.emit("hello_user","hello" + username);
+    socket.broadcast.emit('user_connected', username);
+    return username;
+  }
+
+  @SubscribeMessage('submit_chat')
+  handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.broadcast.emit('new_chat', {
+      chat,
+      username : socket.id,
+    });
   }
 }
